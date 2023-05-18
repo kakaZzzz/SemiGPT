bcf5293de491292a9b9421188923c13d27b9c3b3
lanyu7 <lanyu7@live.com>
2023-05-19 00:36:43 +0800

    update

--- api/main.py
+++ api/main.py
@@ -23,29 +23,40 @@ from langchain.schema import (
 
 # api keys
 import os
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 os.environ["SERPAPI_API_KEY"] = "fe89b1adaaba86f6c8ca6ac3e3524b7c057b915cc2a42c10074f2b0421f7c962"
 os.environ["GOOGLE_CSE_ID"] = "05d376f54d3ac45e5"
 os.environ["GOOGLE_API_KEY"] = "AIzaSyABII3Xyk0eIqj76ebt_P1Hxr0gL0ABDOU"
 
-llm = OpenAI(temperature=0.9)
+llm = ChatOpenAI(temperature=0.9, streaming=True)
 
 system_message_prompt = SystemMessagePromptTemplate.from_template("你是一个有用的助手，能帮助我准确得分解任务")
 
 human_message_prompt = HumanMessagePromptTemplate.from_template("""
-    我想{goal}，请分步骤告诉我怎么做
+    分解任务的时候，要用尽量少的步骤，并且只能用以下3种工具：
+
+    1. search
+    2. run code
+    3. write file
+    4. browser website
+
+    我想{goal}，请分步骤告诉我用什么工具和输入内容是什么，并且一定要用以下格式返回给我：
+    [tool="工具", input="输入内容"]
+
 """)
 
 chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
 chain = LLMChain(llm=llm, prompt=chat_prompt)
 
-print(chat_prompt.format_prompt(goal="快速学习英语").to_messages())
+# print(chat_prompt.format_prompt(goal="快速学习英语").to_messages())
 
 
 app = FastAPI() # 创建API实例
 
 @app.get("/")
 async def root():
-    res = chain.run()
+    res = chain.run({
+        'goal': '北京今天的天气'
+    })
     return {"message": res}
--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
-from langchain.llms import OpenAI
-import json
+import openai
 
-import os
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["SERPAPI_API_KEY"] = "fe89b1adaaba86f6c8ca6ac3e3524b7c057b915cc2a42c10074f2b0421f7c962"
-os.environ["GOOGLE_CSE_ID"] = "05d376f54d3ac45e5"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyABII3Xyk0eIqj76ebt_P1Hxr0gL0ABDOU"
+openai.api_key = "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"
 
-llm = OpenAI(temperature=0)
+res = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
 
-tools = load_tools(["google-search", "llm-math"], llm=llm)
-
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, return_intermediate_steps=True)
-
-response = agent({"input":"梅西和c罗谁更厉害？"})
-
-# print(response["intermediate_steps"])
-
-print(json.dumps(response, indent=2))
-
-
+print(res)
